b906a66257e5fc245f3cd9bd5d5d852e9e8f07ac
Buckysama <offerre@hotmail.com>
2018-05-28 21:00:56 +0700

    Add footer

--- web-project/src/App.js
+++ web-project/src/App.js
@@ -13,13 +13,27 @@ class App extends Component {
     return (
       <div className="App">
       <Navbar />
-      <Carousel />
+      {/* <Carousel /> */}
+
         <div className="App container">
           <Route exact path="/" component={Home} />
           <Route path="/Order" component={Order} />
           <Route path="/Product" component={Product} />
           <Route path="/Contact" component={Contact} />
         </div>
+
+      {/* Footer */}
+      <footer class="w3-center w3-black w3-padding-64 w3-opacity w3-hover-opacity-off">
+        <a href="#home" class="w3-button w3-light-grey"><i class="fa fa-arrow-up w3-margin-right"></i>To the top</a>
+        <div class="w3-xlarge w3-section">
+        <img src="Brand.png" /><br />
+          <a href="https://www.facebook.com/srichand1948" class="w3-button"><i class="fa fa-facebook-official w3-hover-opacity"></i></a>
+          <a href="https://www.instagram.com/srichand1948" class="w3-button"><i class="fa fa-instagram w3-hover-opacity"></i></a>
+          <a href="https://twitter.com/srichand1948" class="w3-button"><i class="fa fa-twitter w3-hover-opacity"></i></a>
+          <a href="https://www.youtube.com/srichandth" class="w3-button"><i class="fa fa-youtube w3-hover-opacity"></i></a>
+        </div>
+        <p>Hotline :(+66)-2300-1661</p>
+      </footer>
       </div>
     );
   }
--- web-project/src/Component/Navbar.js
+++ web-project/src/Component/Navbar.js
@@ -1,6 +1,18 @@
 import React, {Component} from 'react';
 import './Navbar.css'
 
+
+// Change style of navbar on scroll
+window.onscroll = function() {myFunction()};
+function myFunction() {
+    var navbar = document.getElementById("myNavbar");
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        navbar.className = "w3-bar" + " w3-card" + " w3-animate-top" + " w3-white";
+    } else {
+        navbar.className = navbar.className.replace(" w3-card w3-animate-top w3-white", "");
+    }
+}
+
 class Navbar extends Component{
     render(){
         return(
@@ -9,11 +21,11 @@ class Navbar extends Component{
                 <a className="w3-bar-item w3-button w3-hover-black w3-hide-medium w3-hide-large w3-right" href="javascript:void(0);" onClick="toggleFunction()" title="Toggle Navigation Menu">
                 <i className="fa fa-bars"></i>
                 </a>
-                <a href="/" className="w3-bar-item w3-button">HOME</a>
-                <a href="/Order" className="w3-bar-item w3-button w3-hide-small"><i className="fa fa-bars"></i> ORDER</a>
+                <a href="/" className="w3-bar-item w3-button"><img src="logo2.png" width="20" height="20" /></a>
                 <a href="/Product" className="w3-bar-item w3-button w3-hide-small"><i className="fa fa-th"></i> PRODUCT</a>
+                <a href="/Order" className="w3-bar-item w3-button w3-hide-small"><i className="fa fa-bars"></i> ORDER</a>
                 <a href="/Contact" className="w3-bar-item w3-button w3-hide-small"><i className="fa fa-envelope"></i> CONTACT</a>
-                <a href="#" className="w3-bar-item w3-button w3-hide-small w3-right w3-hover-red">
+                <a href="#" className="w3-bar-item w3-button w3-hide-small w3-right w3-hover-green">
                 <i className="fa fa-search"></i>
                 </a>
             </div>
